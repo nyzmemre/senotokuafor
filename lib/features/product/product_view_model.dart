439366765d5c7e3b0ace6a6099a54a77a8b51a81
Mehmet Emre ÖZ <nyz_memre_09@hotmail.com>
2024-04-08 19:34:44 +0300

    productviewmodel some changes

---
+++ lib/features/product/product_view_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ProductViewModel extends ChangeNotifier{
+
+  increaseProductNum(){}
+
+  decraseProductNum(){}
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:senotokuafor/features/product/product_view_model.dart';
 
 import 'features/product/product_view.dart';
 
-void main() {
-  runApp(const SenOtoKuafor());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter('senOtoKuafor');
+
+
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (_)=>ProductViewModel()),
+  ],
+  child: const SenOtoKuafor(),
+  ));
 }
 
 class SenOtoKuafor extends StatelessWidget {
